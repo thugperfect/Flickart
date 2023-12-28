3c11435769b24ab1ec41c642e1e43acdafb62b71
thugperfect <meenamani9444@gmail.com>
2023-12-28 19:48:28 +0530

    filtering according to categories and pagination

--- src/components/HomeBody.js
+++ src/components/HomeBody.js
@@ -13,38 +13,42 @@ import Product from "./Product";
 const HomeBody = () => {
   const [products, setProducts] = useState([]);
   const [modifyingProducts, setModifyingProducts] = useState([]);
-  const [filterCount,setFilterCount] = useState(0);
-  function pagination(no){
-    const changeData = products.filter((k,i)=>{
-        if(i<no*25&&i>=(no-1)*25){
-            return true
-        }
-    })
+  const [filterCount, setFilterCount] = useState(0);
+  function pagination(no) {
+    const changeData = products.filter((k, i) => {
+      if (i < no * 25 && i >= (no - 1) * 25) {
+        return true;
+      }
+    });
 
-    console.log(changeData)
-    setModifyingProducts(changeData)
-    setFilterCount(filterCount+1);
+    console.log(changeData);
+    setModifyingProducts(changeData);
+    setFilterCount(filterCount + 1);
   }
   async function fetchData() {
     const res = await fetch("https://dummyjson.com/products?limit=100");
     const data = await res.json();
-    setProducts(data.products);
+    const shuffledData = data.products.sort(() => Math.random() - 0.5);
+    setProducts(shuffledData);
 
-    const changeData = data.products.filter((k,i)=>{
-        if(i<25){
-            return true
-        }
-    })
+    const changeData = data.products.filter((k, i) => {
+      if (i < 25) {
+        return true;
+      }
+    });
+    const cat = data.products.map((k) => k.category);
+    const catdat = cat.filter((k, i) => {
+      return cat.indexOf(k) === i;
+    });
+    console.log(catdat);
 
-    console.log(changeData)
-    setModifyingProducts(changeData)
+    console.log(changeData);
+    setModifyingProducts(changeData);
   }
   useEffect(() => {
     fetchData();
-    
   }, []);
-
-  
+  const pageArray = [1, 2, 3, 4];
   const catagoryArr = [
     {
       text: "Smartphones",
@@ -59,7 +63,7 @@ const HomeBody = () => {
       element: <GiWatch className="text-[40px]" />,
     },
     {
-      text: "Mens shoes",
+      text: "Shoes",
       element: <GiConverseShoe className="text-[40px]" />,
     },
     {
@@ -75,13 +79,23 @@ const HomeBody = () => {
       element: <GiSunglasses className="text-[40px]" />,
     },
   ];
+  const filterFn = (text) => {
+    const filterCategories = products.filter((k) => {
+      return k.category.includes(text.toLowerCase());
+    });
+    setModifyingProducts(filterCategories);
+  };
 
   return (
     <div>
       <div className=" h-[150px] bg-white m-3 flex justify-center items-center">
         <div className="w-[80%] h-full flex justify-center items-center">
-          {catagoryArr.map((k,i) => (
-            <div key={i} className="m-2 flex flex-col items-center cursor-pointer">
+          {catagoryArr.map((k, i) => (
+            <div
+              key={i}
+              onClick={() => filterFn(k.text)}
+              className="m-2 flex flex-col items-center cursor-pointer"
+            >
               <div className="">{k.element}</div>
               <div className="">{k.text}</div>
             </div>
@@ -89,12 +103,24 @@ const HomeBody = () => {
         </div>
       </div>
       <div className=" h-500px bg-white m-3 flex gap-2 flex-wrap justify-center p-3">
-     
-        {modifyingProducts.length>0 ?
-        modifyingProducts.map((k,i) => (
-          <Product key={k.id} data={k} />
-        )):""}
+        {modifyingProducts.length > 0
+          ? modifyingProducts.map((k, i) => <Product key={k.id} data={k} />)
+          : ""}
       </div>
+      <div className="flex justify-center">
+        {modifyingProducts.length === 25 ? (
+          <div className="flex bg-white">
+            {pageArray.map((k) => (
+              <div onClick={()=>pagination(k)} className="w-[40px] h-[40px] flex justify-center items-center cursor-pointer outline outline-1">
+                {k}
+              </div>
+            ))}
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
+      <div className="h-[20px] w-full"></div>
     </div>
   );
 };
--- src/components/Product.js
+++ src/components/Product.js
@@ -6,8 +6,8 @@ const Product = ({data}) => {
     <img src={data.thumbnail} className="w-[250px] h-[250px] rounded-t-lg bg-slate-300" alt={data.name}/>
     <div className="flex flex-col p-2">
     <div className="font-bold flex justify-center m-2">{data.brand}</div>  
-    <div className="text-lg flex justify-center">{data.title.length>25?data.title.slice(0,22):data.title}</div>
-    <div className="text-md flex justify-center">{data.description.length>50?data.description.slice(0,50):data.description}...</div>
+    <div className="text-lg flex justify-center">{data.title.length>22?data.title.slice(0,22)+"...":data.title}</div>
+    <div className="text-md flex justify-center">{data.description.length>45?data.description.slice(0,45)+"...":data.description}</div>
     <div className="flex justify-center items-end gap-2">
     <div className="flex  text-xl font-semibold">{data.price} $</div>
     <div className="text-gray-500 text-sm line-through">{Math.floor(data.price+((data.discountPercentage*data.price)/100))} $</div>
